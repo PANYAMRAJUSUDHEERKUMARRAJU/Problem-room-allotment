7a52d9b1c110230df96cf670f92eb473c687883e
PANYAMRAJUSUDHEERKUMARRAJU <133593546+PANYAMRAJUSUDHEERKUMARRAJU@users.noreply.github.com>
2024-06-25 08:55:49 +0530

    Update Room_Allotment_Program.cpp

--- Room_Allotment_Program.cpp
+++ Room_Allotment_Program.cpp
@@ -1,152 +1,124 @@
-#include <iostream>
-#include <regex>
-using namespace std;
+    // show line to distinguish again
+        design('#');
 
-bool isValid(string s)
-{
-  const regex pattern("(0|91)?[7-9][0-9]{9}");
-  if(regex_match(s, pattern))
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+        hotel[0].position(a);  // also tells that how many chairs are still empty 
+
+        int n = 1;
+// we have 10 rows & 4 columns, showing the complete view of chairs
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                n++;
+                if (strcmp(hotel[n].chair[i][j], "Empty") != 0)
+                {
+                    // -cout<<endl<<" The chair no."<<(n-1)<<"is reserved for : "<<hotel[a].chair[i][j]<<".";
+                }
+            }
+        }
+        break;
+    }
+// entered room number is not correct
+    if (a > q)
+        cout << "Enter correct room number :";
 }
 
-        void Book(){
-            cout<<"\n\t\t\t*******Book a Room*******\t\t\t\n";
-            char Name[50];
-            cout<<"Enter the customer's Name: ";
-            cin.ignore();
-            cin.getline(Name,50);
-            
-            char Address[50];
-            cout<<"Enter the customer's Address: ";
-            cin.ignore();
-            cin.getline(Address,50);
-            
-            string phone;
-            cout<<"Enter the customer's Mobile No.: ";
-            cin>>phone;
-            
-            if(isValid(phone)){
-               cout << "Your Mobile No. is Varified..\n";
-            }else{
-                cout<<"Invalid Mobile No. Please Enter valid Mobile No.\n";
-                cout<<"Enter the customer's Mobile No.: ";
-                cin>>phone;
-            }
-            
-            string Area;
-            cout<<"In which Area do you want to book your room: ";
-            cin>>Area;
-            cout<<"\n\t\t\tcongratulations!!...Your room has Booked\t\t\t\n";
-            cout<<endl;
-        }
-        void setHost(){
-            cout<<"\n\t\t\t*******Set the host*******\t\t\t\n";
-            char Name[50];
-            cout<<"Host's Name To Manage the room: ";
-            cin.ignore();
-            cin.getline(Name,50);
-            cout<<endl;
-        }
-        
-        void startTime(){
-            cout<<"\n\t\t\t*******Set the start time*******\t\t\t\n";
-            char Time[50];
-            cout<<"Set the start time to start the work: ";
-            cin.ignore();
-            cin.getline(Time,50);
-            cout<<"At "<<Time<<" Everyone should start their work\n";
-            
-            char Time1[50];
-            cout<<"Lunch break: ";
-            cin.ignore();
-            cin.getline(Time1,50);
-            cout<<"At "<<Time1<<" you Can Lunch in Lunch Area\n";
-            cout<<endl;
-        }
-        
-        void endTime(){
-            cout<<"\n\t\t\t*******Set the end time*******\t\t\t\n";
-            char Time[50];
-            cout<<"Set the end time to stop the work: ";
-            cin.ignore();
-            cin.getline(Time,50);
-            cout<<"At "<<Time<<" Relax!!";
-            cout<<endl;
-        }
-        
-        void capability(){
-            cout<<"\n\t\t\t*******Set the chair booking capability*******\t\t\t\n";
-            int n1,n2,n3;
-            cout<<"Enter chair capability in Workspace Area: ";
-            cin>>n1;
-            cout<<"There are total "<<n1<<" employees so,Everyone should be comforatable in Workspace Area\n";
-            cout<<"Enter chair capability in Meeting Area: ";
-            cin>>n2;
-            cout<<n2<<" Number of people can meet in Meeting Area\n";
-            cout<<"Enter chair capability in Lab Area: ";
-            cin>>n3;
-            cout<<n3<<" employees perform experiment in Lab Area\n";
-            cout<<endl;
-        }
-        
-        void roomNo(){
-            cout<<"\n\t\t\t*******Define the room number*******\t\t\t\n";
-            cout<<"According to Your All requirement\n";
-            cout<<"Room is divided in two Compartment: \n";
-            cout<<"\n\t\t\t---------------FIRST COMPARTMENT---------------\t\t\t\n";
-            cout<<"As you Enter in room There is stuff Area in Room No.101\n";
-            cout<<"There is Kitchen Area after the stuff and Room No. of Kitchen is 102\n";
-            cout<<"There is coffee Room after Kitchen Room and Room No. of coffee Area is 103\n";
-            cout<<"At back side There is small lean Area\n";
-            cout<<"So finally..\n";
-            cout<<"Room No. of stuff Area: 101\n";
-            cout<<"Room No. of Kitchen Area: 102\n";
-            cout<<"Room No. of coffee Area: 103\n";
-            cout<<"\n\t\t\t---------------SECOND COMPARTMENT---------------\t\t\t\n";
-            cout<<"Room No. of Lab Area: 201\n";
-            cout<<"Room No. of Meeting Area: 202\n";
-            cout<<"Room No. of Workspace Area: 203\n";
-        }
+// to look for every position & used in show function
+void reservation::position(int l)
+{
+    int s = 0, p = 0;    // s for serial number & p for counting empty chairs
 
-int main(){
-        cout<<"\n1.Book a room";
-        cout<<"\n2.Set the host";
-        cout<<"\n3.Set the start time";
-        cout<<"\n4.Set the end time";
-        cout<<"\n5.Set the chair booking capability";
-        cout<<"\n6.Define the room number\n";
-        
-    int ch;
-    do{
-        cout<<"\nEnter Your Choice: ";
-        cin>>ch;
-        
-        switch(ch){
-            case 1:
-            Book();
-            break;
-            case 2:
-            setHost();
-            break;
-            case 3:
-            startTime();
-            break;
-            case 4:
-            endTime();
-            break;
-            case 5:
-            capability();
-            break;
-            case 6:
-            roomNo();
-            break;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << endl;
+
+        for (int j = 0; j < 4; j++)
+        {
+            s++;
+
+            // if chair is empty 
+            if (strcmp(hotel[l].chair[i][j], "Empty") == 0)
+            {
+                cout.width(5);       // space between chair
+                cout.fill(' ');
+                cout << s << ".";    // serial no of chair
+                cout.width(10);
+                cout.fill(' ');
+                cout << hotel[l].chair[i][j];   // if chair is empty then show empty
+                p++;    // count the number of empty chairs 
+            }
+// if chair is not empty the again show space but this time show traveller's name
+            else
+            {
+                cout.width(5);
+                cout.fill(' ');
+                cout << s << ".";
+                cout.width(10);
+                cout.fill(' ');
+                cout << hotel[l].chair[i][j];
+            }
         }
-    }while(ch!=0);
+    }
+    cout << "\n\nThere are " << p << " Chairs empty in Room No: " << hotel[l].room_number;
+}
+
+// show the installing data of room 
+void reservation::avail()
+{
+    for (int n = 0; n < q; n++)
+    {
+        design('#');
+        cout << "Room no: \t" << hotel[n].room_number << "\nHost: \t" << hotel[n].host << "\t\tStart time: \t" << hotel[n].start << "\tEnd  Time: \t"
+             << hotel[n].send << "\nFrom: \t\t" << hotel[n].from << "\t\tTo: \t\t\t"
+             << hotel[n].to << "\n";
+        design('#');
+        design('_');
+    }
+}
+
+int main()
+{
+    system("cls");
+    int w;
+    
+    while (1)
+    {
+        // system("cls");
+        cout << "\n\n\n\n\n";
+        design('#');
+        cout << "\n\n";
+        cout<<"\t\t\t\t\t\t\t\t\t Hotel Room Booking System";
+        cout << "\n\n\n\t\t\t\t\t\t1.Booking\n\t\t\t\t\t\t"
+             << "2.Reservation\n\t\t\t\t\t\t"
+             << "3.Show\n\t\t\t\t\t\t"
+             << "4.Rooms Available. \n\t\t\t\t\t\t"
+             << "5.Exit";
+
+        cout << "\n\t\t\t\t\t\tEnter your choice:-> ";
+        cin >> w;    // our choice
+
+        switch (w)
+        {
+
+        case 1:
+            hotel[q].booking();     // installed the room & take data
+            break;
+
+        case 2:
+            hotel[q].allotment();    // if entered room is already installed then allotment starts by taking chairs
+            break;
+
+        case 3:
+            hotel[0].display();      // show the complete chair's view of required room
+            break;
+
+        case 4:
+            hotel[0].avail();    // show the complete installing data
+            break;
+
+        case 5:
+            exit(0);    // exit from the room 
+        }
+    }
     return 0;
 }
